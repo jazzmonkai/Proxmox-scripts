b4b32fa4b925e6792a7910b9b4d370a1255b7145
jazzmonkai <88624398+jazzmonkai@users.noreply.github.com>
2023-02-03 20:43:41 +0000

    Create autologon.sh

---
+++ autologon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /etc/systemd/system/getty@tty1.service.d/
+
+nano /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin root %I $TERM
+EOF
+
+systemctl edit container-getty@.service <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 $TERM
+EOF
+
+reboot
